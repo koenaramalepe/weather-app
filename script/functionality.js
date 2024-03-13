85b5d104d4a0c16fb8a265dbbcf2aadae98c7754
koena ramalepe <koenaramalepe2@gmail.com>
2024-03-13 17:18:10 +0200

    Added functionality to the function handling response

--- script/functionality.js
+++ script/functionality.js
@@ -1,18 +1,25 @@
+function updateWeatherData(response){   
+    let temperatureElement = document.querySelector("#temperature")
+    temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+    let displayedCity = document.querySelector("#city-name");
+    displayedCity.innerHTML = response.data.city
+    
+}
+
 function searchCity(city){
     let apiKey = "b43bc25t07dffbba80b4fec67770a5o8";
     let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-    console.log(apiUrl);
+    axios.get(apiUrl).then(updateWeatherData);
    
 }
 function handlingResponse(event){
     event.preventDefault();
-    let cityValue = document.querySelector("#search-input").value;
-    console.log(cityValue)
-    // document.querySelector("#city-name").textContent = cityValue.value
-    let displayedCity = document.querySelector("#city-name");
-    displayedCity.innerHTML = cityValue.value;
-    searchCity(cityValue);
+    let cityValue = document.querySelector("#search-input");
+    
+    searchCity(cityValue.value);
 }
 
 let formInput = document.querySelector("#form1");
 formInput.addEventListener("submit", handlingResponse);
+
+searchCity("Polokwane");
