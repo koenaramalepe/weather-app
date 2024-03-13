480101f2afaed7a7643643582d085af4dc508502
koena ramalepe <koenaramalepe2@gmail.com>
2024-03-13 12:25:42 +0200

    added new function that takes in city as a parameter

--- script/functionality.js
+++ script/functionality.js
@@ -1,14 +1,18 @@
+function searchCity(city){
+    let apiKey = "b43bc25t07dffbba80b4fec67770a5o8";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    console.log(apiUrl);
+   
+}
 function handlingResponse(event){
     event.preventDefault();
-
-    let cityValue = document.querySelector("#search-input");
+    let cityValue = document.querySelector("#search-input").value;
+    console.log(cityValue)
     // document.querySelector("#city-name").textContent = cityValue.value
     let displayedCity = document.querySelector("#city-name");
     displayedCity.innerHTML = cityValue.value;
-
-
+    searchCity(cityValue);
 }
-
 
 let formInput = document.querySelector("#form1");
 formInput.addEventListener("submit", handlingResponse);
